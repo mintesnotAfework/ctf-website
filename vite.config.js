367d10f0fc13c8e645e612f40c08b53905c331a0
mintesnot afework <152774684+mintesnotAfework@users.noreply.github.com>
2024-09-16 01:32:53 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  port : 80
+  server : {
+    host: '0.0.0.0',
+    port : 3000
+  },
 })
